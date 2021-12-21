408cad1055dfd84496660b572f729a65e3909a7b
lukmanx <archer.lukman@gmail.com>
2021-12-22 06:14:07 +0700

    -  create two dimension slice example

---
+++ bionic_bootcamp/two_d_slices.go
@@ -0,0 +1,40 @@
+package main
+import (
+	"fmt"
+)
+
+/*
+	{
+		{x,x,x,x}, <== 2 rows, 4 columns
+		{y,y,y,y}
+	}
+*/
+
+//2D slices
+var two_d_slices = [][] int { {1,2,3}, {4,5,6} }
+
+var rows int = 3
+var cols int = 5
+var tes2d = make([][]int, rows)
+
+func main() {
+	fmt.Println(tes2d)
+	var i int
+	/*
+	for i = range(tes2d) {
+		if (i == 1) {
+			tes2d[i] = []int{1,2,3}
+		} else {
+			tes2d[i] = []int{0,0,0}
+		}
+	}
+	*/
+	for i = range(tes2d) {
+		tes2d[i] = make( []int, cols )
+	}
+	fmt.Println(tes2d)
+}
+
+
+
+
