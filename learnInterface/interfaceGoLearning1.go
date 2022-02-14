ec18f603ff5e9a232fef73b2974aa5fc50d52a29
lukman aul <archer.lukman@gmail.com>
2022-02-14 09:25:17 +0700

    Create interfaceGoLearning1.go

---
+++ learnInterface/interfaceGoLearning1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Human interface {
+	Eating(arg string) bool
+}
+type Muslim struct {
+	name, religion string
+}
+type Kristen struct {
+	name, religion string
+}
+
+func (m *Muslim) Eating(arg string) bool {
+	var result bool
+	if m.religion == "muslim" {
+		if arg == "babi" {
+			result = false
+		} else {
+			result = true
+		}
+	}
+	return result
+}
+
+func (k *Kristen) Eating(arg string) bool {
+	return true
+
+}
+
+func main() {
+	doni := &Muslim{"doni", "muslim"}
+	robert := &Kristen{"robert", "kristen"}
+
+	test_doni := doni.Eating("ayam")
+	test_robert := robert.Eating("babi")
+	fmt.Println(test_doni, test_robert)
+}
